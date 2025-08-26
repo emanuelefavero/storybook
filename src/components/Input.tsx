d6dad6e6e7e32b80802435259172ac725fe2b3bd
Emanuele Favero <emafaux89@gmail.com>
2025-08-26 15:40:57 +0200

    feat: add variant to Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,38 @@
 import { cn } from '@/lib/utils'
+import { cva } from 'class-variance-authority'
 
-type Props = React.ComponentPropsWithRef<'input'>
+type Props = React.ComponentPropsWithRef<'input'> & {
+  variant?: 'default' | 'primary' | 'error' | 'warning' | 'success'
+}
 
-export default function Input({ className, type = 'text', ...props }: Props) {
+const variants = cva(
+  'flex w-full rounded-lg border px-3 py-2 text-base ring-offset-(--background) placeholder:text-gray-500 focus-visible:ring-1 focus-visible:ring-(--foreground) focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'border-(--foreground) bg-gray-500/5',
+        primary: 'border-blue-500 bg-blue-500/5',
+        error: 'border-red-500 bg-red-500/5',
+        warning: 'border-yellow-500 bg-yellow-500/5',
+        success: 'border-green-500 bg-green-500/5',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
+export default function Input({
+  variant,
+  className,
+  type = 'text',
+  ...props
+}: Props) {
   return (
     <input
       type={type}
-      className={cn(
-        'border-input placeholder:text-muted-(--foreground) focus-visible:ring-ring flex w-full rounded-md border bg-transparent px-3 py-2 text-base ring-offset-(--background) file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-(--foreground) focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
+      className={cn(variants({ variant }), className)}
       {...props}
     />
   )
